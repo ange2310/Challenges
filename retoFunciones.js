666116ab45461dbad4666a1ff3561bc348114faf
Angelica Marcillo <angelicamariaalba049@gmail.com>
2025-02-12 11:20:19 -0500

    Agregué el reto

---
+++ retoFunciones.js
@@ -0,0 +1,14 @@
+//Angelica Marcillo
+
+//Reto: Hacer que la funcion reciba un número y determine si es par o impar
+
+//Regular Functions
+
+function numPar(num){
+    if(num%2 == 0){
+        console.log(`El número ${num} es par`)
+    } else{
+        console.log(`El número ${num} es impar`)
+    }
+}
+numPar()
