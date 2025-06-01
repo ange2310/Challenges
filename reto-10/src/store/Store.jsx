9c95d34b7efa469b8a26ff0909502e5e644c1635
ange2310 <168590717+ange2310@users.noreply.github.com>
2025-06-01 17:22:53 -0500

    Delete reto-10 directory

--- reto-10/src/store/Store.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from './Slices/counterSlice';
-import stackReducer from './Slices/stackSlice';
-export const store = configureStore({
-    reducer: {
-      counter: counterReducer,
-      stack: stackReducer
-    },
-  });
